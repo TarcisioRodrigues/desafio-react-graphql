5f9ccd16b51f3513b7bb2ef8b7a45fbd6017bbd5
tarcisiorodrigues454@gmail.com <TarcisioRodrigues>
2024-02-06 23:10:32 -0300

    feat:columns and tasks

--- desafio/src/src/components/PieChart.tsx
+++ desafio/src/src/components/PieChart.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Chart from "react-apexcharts";
 import { useTodoContext } from "../../context/todoContext"
 export const PieChart: React.FC = () => {
-  const {tasksCount}=useTodoContext()
+  const {tasksCount,columns}=useTodoContext()
   
-  console.log(tasksCount)
+ 
   const options: ApexCharts.ApexOptions = {
-    labels: ["A Fazer", "Em progresso", "Finalizado"],
+    labels:columns,
   };
 
   return (
